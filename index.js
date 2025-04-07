cb445d25bf7a234a221742d13506a0557c902bf9
fenil1503 <fenil@technoproject.in>
2025-04-07 13:46:49 +0530

    changes

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  console.log("API endpoint:", req.originalUrl);
+  next();
+});
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
